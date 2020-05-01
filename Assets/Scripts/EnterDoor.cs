6c94ad2bd5e8ea2409ac950457bc97651d41479d
Kyle O'Dell <kodell315@gmail.com>
2020-04-30 20:57:04 -0700

    added tracks to folders

---
+++ Assets/Scripts/EnterDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : MonoBehaviour
+{
+    public AudioClip notEnoughOrbs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            if (Player.Instance.orbsInPossession >= 10)
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+            else 
+            {
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.clip = notEnoughOrbs;
+                audio.Play();
+            }
+        }
+    }
+}
