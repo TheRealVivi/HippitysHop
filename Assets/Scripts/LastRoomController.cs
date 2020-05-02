89b77448842e7793e33a0458dbaca8810699f90a
Kyle O'Dell <kodell315@gmail.com>
2020-05-01 22:23:16 -0700

    added script to plug in orbs

--- Assets/Scripts/LastRoomController.cs
+++ Assets/Scripts/LastRoomController.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class LastRoomController : MonoBehaviour
 {
     public GameObject tenOrbCollection;
+    public GameObject threeOrbCollection;
+    public GameObject firstOrb;
+    public GameObject secondOrb;
+    public GameObject thirdOrb;
     public float usableDistance = 9f;
+    public float currentDistance;
+    bool tenOrbUsed;
+    bool threeOrbUsed;
 
     // Start is called before the first frame update
     void Start()
     {
         tenOrbCollection.SetActive(false);
+        firstOrb.SetActive(false);
+        secondOrb.SetActive(false);
+        thirdOrb.SetActive(false);
+        tenOrbUsed = false;
+        threeOrbUsed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = this.transform.position - Player.Instance.transform.position;
+        Vector3 tenorbdirection = tenOrbCollection.transform.position - Player.Instance.transform.position;
+        Vector3 threeorbdirection = threeOrbCollection.transform.position - Player.Instance.transform.position;
 
-        if (Input.GetKeyDown(KeyCode.E) && direction.sqrMagnitude < usableDistance) 
+        if (Input.GetKeyDown(KeyCode.E) && (currentDistance = tenorbdirection.sqrMagnitude) < usableDistance && !tenOrbUsed) 
         {
             tenOrbCollection.SetActive(true);
             Player.Instance.orbsInPossession -= 10;
+            tenOrbUsed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && threeorbdirection.sqrMagnitude < usableDistance && tenOrbUsed && !threeOrbUsed) 
+        {
+            firstOrb.SetActive(Player.Instance.orbsInPossession >= 1);
+            secondOrb.SetActive(Player.Instance.orbsInPossession >= 2);
+            thirdOrb.SetActive(Player.Instance.orbsInPossession >= 3);
+            Player.Instance.orbsInPossession = 0;
+            threeOrbUsed = true;
         }
     }
 }
