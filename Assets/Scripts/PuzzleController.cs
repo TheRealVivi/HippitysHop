b666b9a201ba6866d24df9a1f8005957c855f42b
Kyle O'Dell <kodell315@gmail.com>
2020-05-01 18:44:40 -0700

    made hall prefabs, added halls to level01, added spring joint platforms

---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    public GameObject fire1, fire2, wallFire1, wallFire2, wall;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire1.SetActive(false);
+        fire2.SetActive(false);
+        wallFire1.SetActive(false);
+        wallFire2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
