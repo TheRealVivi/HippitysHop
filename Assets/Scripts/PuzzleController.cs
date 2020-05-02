d552558d234b34085aea4fce6636d229e10ecd02
Kyle O'Dell <kodell315@gmail.com>
2020-05-01 20:13:24 -0700

    updated switch script, updated puzzleController

--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PuzzleController : MonoBehaviour
 {
     public GameObject fire1, fire2, wallFire1, wallFire2, wall;
+    public Switch switch1, switch2;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,11 @@ public class PuzzleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fire1.SetActive(switch1.activated);
+        wallFire1.SetActive(switch1.activated);
+        fire2.SetActive(switch2.activated);
+        wallFire2.SetActive(switch2.activated);
+
+        wall.SetActive(!switch1.activated || !switch2.activated);
     }
 }
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool activated;
+    public float usableDistance = 9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = this.transform.position - Player.Instance.transform.position;
+        if (Input.GetKey(KeyCode.E) && direction.sqrMagnitude < usableDistance) 
+        {
+            activated = true;
+        }
+    }
+}
