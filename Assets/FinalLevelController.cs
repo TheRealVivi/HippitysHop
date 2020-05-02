9011f80d508fbf801e542562e07a091f200244a7
Kyle O'Dell <kodell315@gmail.com>
2020-05-02 02:12:21 -0700

    began building final level, made specific enemy prefabs, used polymorphism to make Enemy script a child of Character, and Hippity a child of Enemy.

---
+++ Assets/FinalLevelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.Instance.transform.position.y < -5f)
+            Player.Instance.hp = 0;
+    }
+}
