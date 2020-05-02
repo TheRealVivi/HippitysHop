8690f9decb70f5290bac3b923f6ac2f5afed1ab1
Kyle O'Dell <kodell315@gmail.com>
2020-05-01 21:53:55 -0700

    added last room to 2nd level

---
+++ Assets/Scripts/LastRoomController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastRoomController : MonoBehaviour
+{
+    public GameObject tenOrbCollection;
+    public float usableDistance = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tenOrbCollection.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = this.transform.position - Player.Instance.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.E) && direction.sqrMagnitude < usableDistance) 
+        {
+            tenOrbCollection.SetActive(true);
+            Player.Instance.orbsInPossession -= 10;
+        }
+    }
+}
