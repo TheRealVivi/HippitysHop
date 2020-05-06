4e5440ab9c30f6db96072cdd74c58b386616b0a2
Kyle O'Dell <kodell315@gmail.com>
2020-05-05 22:59:24 -0700

    added three animations, added animation script, updated executables

---
+++ Assets/Scripts/HippityAllyAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HippityAllyAnimator : MonoBehaviour
+{
+    public Animator animator;
+    public bool fourOrbs;
+    public bool tenOrbs;
+
+    private void Start()
+    {
+        fourOrbs = false;
+        tenOrbs = false;
+    }
+    private void Update()
+    {
+        fourOrbs = Player.Instance.orbsInPossession >= 4;
+        tenOrbs = Player.Instance.orbsInPossession >= 10;
+        animator.SetBool("fourOrbs", fourOrbs);
+        animator.SetBool("tenOrbs", tenOrbs);
+    }
+
+}
